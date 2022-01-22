15d69e2434009097517605de54970f42233a7630
guifgomes47 <90487692+guifgomes47@users.noreply.github.com>
2022-01-21 21:00:19 -0300

    formating the code - none line was changed

--- script.js
+++ script.js
@@ -1,12 +1,10 @@
 const Modal = {
-    //Add class active in modal
-    open(){
-        document.querySelector('.modal-overlay')
-        .classList.add('active')
-    },
-    //Close class active in modal
-    close(){
-        document.querySelector('.modal-overlay')
-        .classList.remove('active')
-    }
-}
+  //Add class active in modal
+  open() {
+    document.querySelector(".modal-overlay").classList.add("active");
+  },
+  //Close class active in modal
+  close() {
+    document.querySelector(".modal-overlay").classList.remove("active");
+  },
+};
